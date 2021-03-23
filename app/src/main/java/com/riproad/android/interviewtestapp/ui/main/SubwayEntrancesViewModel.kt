05e1590a819a54190684fb55835ed5db47906aa6
Chris Vansco <chris.vansco@gmail.com>
2021-03-23 10:36:32 -0400

    Cleaned code

--- app/src/main/java/com/riproad/android/interviewtestapp/ui/main/SubwayEntrancesViewModel.kt
+++ app/src/main/java/com/riproad/android/interviewtestapp/ui/main/SubwayEntrancesViewModel.kt
@@ -2,13 +2,8 @@ package com.riproad.android.interviewtestapp.ui.main
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
-import com.riproad.android.interviewtestapp.model.SubwayEntrancesResponse
 import com.riproad.android.interviewtestapp.util.State
 import kotlinx.coroutines.Dispatchers
-import okhttp3.RequestBody
-import org.json.JSONObject
-import java.util.*
-import kotlin.collections.HashMap
 
 class SubwayEntrancesViewModel(private val subwayEntrancesRepository: SubwayEntrancesRepository) : ViewModel() {
     fun getSubwayEntrances() = liveData(Dispatchers.IO) {
@@ -17,7 +12,7 @@ class SubwayEntrancesViewModel(private val subwayEntrancesRepository: SubwayEntr
             val timestamp = System.currentTimeMillis()
             val json = HashMap<String, Long>()
             json["timestamp"] = timestamp
-            emit(State.Success(subwayEntrancesRepository.getSubwayEntrances(json) as SubwayEntrancesResponse))
+            emit(State.Success(subwayEntrancesRepository.getSubwayEntrances(json)))
         } catch (exception: Exception) {
             emit(State.Failure(exception))
         }
